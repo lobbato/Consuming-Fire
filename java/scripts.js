c9780f8c76c08ad6ec419f75d49df3810cadb44a
lobbato <emily.lobbato@gmail.com>
2020-12-01 18:16:26 -0800

    insurance?

--- java/scripts.js
+++ java/scripts.js
@@ -1,7 +1,32 @@
+//Over 21
 $(document).ready(function() {
-    const age = parseInt(prompt("How old are you?"));
+    const over21 = confirm("Are you over 21? Click OK for yes or Cancel for no.");
   
-    if (age >= 21) {
+    if (over21) {
       $('#drinks').show();
+    } else {
+      $('#under-21').show();
     }
+  });
+
+//Insurance 
+$(document).ready(function() {
+    $("form#insurance").submit(function(event) {
+      const age = parseInt($("input#age").val());   // Asks for age of person
+      const gender = $("select#gender").val();      //Asksfor gender of person
+  
+      if (age) {
+        let quote = (100 - age) * 3;
+        if (gender === 'male' && age < 26) {        //Finds the fit of specific demographic (male and under 26)
+          quote += 50;
+        }
+  
+        $("#rate").text(quote);
+        $("#quote").show();
+      } else {
+        alert('Please enter your age.');            //Alert if an age is not added to the equation --> If this is not here the non-respone would be treated as a "0"
+      }
+  
+      event.preventDefault();
+    });
   });
