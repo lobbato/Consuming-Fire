b83b129563fba16817fb776fa6ac2ab456e1d8f6
lobbato <emily.lobbato@gmail.com>
2020-12-01 18:22:38 -0800

    CAlculator radio button if else contraints

--- java/scripts.js
+++ java/scripts.js
@@ -38,10 +38,16 @@ $(document).ready(function() {
       const number1 = parseInt($("#input1").val());
       const number2 = parseInt($("#input2").val());
       const operator = $("input:radio[name=operator]:checked").val();
-      console.log("1st number: " + number1);  // for debugging
-      console.log("2nd number: " + number2);  // for debugging
-      console.log("operator: " + operator);  // for debugging
-      const result = add(number1, number2);
+      let result;
+      if (operator === "add") {
+        result = add(number1, number2);
+      } else if (operator === "subtract") {
+        result = subtract(number1, number2);
+      } else if (operator === "multiply") {
+        result = multiply(number1, number2);
+      } else if (operator === "divide") {
+        result = divide(number1, number2);
+      }
       $("#output").text(result);
     });
   });
