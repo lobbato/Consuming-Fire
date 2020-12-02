ae4dc0e8b18a13b4e7ca73a153c4c82c4a5cde5f
lobbato <emily.lobbato@gmail.com>
2020-12-01 18:21:00 -0800

    Calculator de-bugging help things

--- java/scripts.js
+++ java/scripts.js
@@ -38,6 +38,9 @@ $(document).ready(function() {
       const number1 = parseInt($("#input1").val());
       const number2 = parseInt($("#input2").val());
       const operator = $("input:radio[name=operator]:checked").val();
+      console.log("1st number: " + number1);  // for debugging
+      console.log("2nd number: " + number2);  // for debugging
+      console.log("operator: " + operator);  // for debugging
       const result = add(number1, number2);
       $("#output").text(result);
     });
